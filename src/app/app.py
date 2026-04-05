9b4f2c8a1b1ab87ec1148775a1d8e1a17bd09be2
Arthur <dragon5552017@gmail.com>
2026-04-05 15:19:59 +0300

    1

--- src/app/app.py
+++ src/app/app.py
@@ -4,7 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-     return 'Hello, World!'
+    print("Request received!")  # Добавьте эту строку
+    return 'Hello, World!'
 
 if __name__ == "__main__":
-     app.run(debug=True, host='0.0.0.0', port = 5001)
+    app.run(debug=True, host='0.0.0.0', port=5001)
